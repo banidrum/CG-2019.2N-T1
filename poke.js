14e2b344130198b6c5730a106b4a3c9a35d855ce
Daniel Brum <dani3544@gmail.com>
2019-12-08 18:49:26 -0300

    Loader não deu mais erros, porém a tela está preta

--- poke.js
+++ poke.js
@@ -1,6 +1,3 @@
-import * as THREE from './lib/threejs/build/three.module';
-import { GLTFLoader } from './lib/threejs/examples/jsm/loaders/GLTFLoader';
-
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
@@ -10,6 +7,6 @@ document.body.appendChild(renderer.domElement);
 
 var loader = new THREE.GLTFLoader();
 
-loader.load('./assets/Squirtle/Squirtle.gltf') {
+loader.load('./assets/Squirtle/Squirtle.gltf', function(gltf) {
     scene.add(gltf.scene);
-}
+});
