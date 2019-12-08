a071da11032dc4e0f6bb95e51afaef3eea5cd950
Daniel Brum <dani3544@gmail.com>
2019-12-08 20:02:23 -0300

    Alteração da cor do fundo, squirtle ainda não aparece

--- poke.js
+++ poke.js
@@ -1,12 +1,36 @@
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+camera.position.set( 1, 1, 20 );
 
 var renderer = new THREE.WebGLRenderer();
+renderer.setClearColor( 0xC5C5C3 );
+renderer.setPixelRatio( window.devicePixelRatio );
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
+
+var ambientLight = new THREE.AmbientLight( 0xcccccc );
+scene.add( ambientLight );
+			
+var directionalLight = new THREE.DirectionalLight( 0xffffff );
+directionalLight.position.set( 0, 1, 1 ).normalize();
+scene.add( directionalLight );			
 
 var loader = new THREE.GLTFLoader();
 
 loader.load('./assets/Squirtle/Squirtle.gltf', function(gltf) {
-    scene.add(gltf.scene);
+    var object = gltf.scene;				
+	gltf.scene.scale.set( 2, 2, 2 );			   
+	gltf.scene.position.x = 0;				    //Position (x = right+ left-) 
+        gltf.scene.position.y = 0;				    //Position (y = up+, down-)
+	gltf.scene.position.z = 0;				    //Position (z = front +, back-)
+	
+	scene.add( gltf.scene );
+}, undefined, function(error) {
+    console.error(error);
 });
+
+function render() {
+    renderer.render(scene, camera);
+}
+
+render();
